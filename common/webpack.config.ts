4771a172d323c7289019a2c2e9c865d92d14eaee
Abner Oliveira <abner.silva@gmail.com>
2017-04-27 20:16:30 -0300

    fixed webpack call for sample app

--- common/webpack.config.ts
+++ common/webpack.config.ts
@@ -1,3 +1,9 @@
 import { getBuildConfig} from './build-config';
 import path = require('path');
-export default getBuildConfig('pratico-ngx.umd.js', 'PraticoNgx', path.join(__dirname, '../src/index.ts'));
+export default getBuildConfig({
+  outputFileName: 'pratico-ngx.umd.js',
+  libraryName: 'PraticoNgx',
+  entryFile: path.join(__dirname, '../src/index.ts'),
+  outputPath: path.join(__dirname, '../dist'),
+  tsConfigPath: path.join(__dirname, '../tsconfig.json')
+});
